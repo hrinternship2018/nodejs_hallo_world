b90a23d0078114c23ea0a29c6e35edfb869f0d64
Ruofan He <hrinternship2018@gmail.com>
2019-04-12 01:00:01 +0900

    Change Files

--- artifact/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const example_1 = require("./example");
-function main() {
-    example_1.example();
-}
-exports.main = main;
-//# sourceMappingURL=app.js.map
